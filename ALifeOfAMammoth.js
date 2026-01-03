3fb96b170e20e78590901e24abc1da42cb22e037
Atos016 <atosgrego82@gmail.com>
2026-01-03 05:58:42 -0300

    Refactor comments and improve code readability

--- ALifeOfAMammoth.js
+++ ALifeOfAMammoth.js
@@ -1,3 +1,5 @@
+// Mamute Mod for Sandboxels
+
 elements.mamute = {
     color: ["#6b4f2a", "#7a5a35", "#5e4525"],
     behavior: behaviors.LIFE,
@@ -8,26 +10,26 @@ elements.mamute = {
     stateHigh: "carne_de_mamute",
     tick: function(pixel) {
 
-        // morrer no calor
+        // Morrer no calor
         if (pixel.temp > 35 && Math.random() < 0.05) {
-            changePixel(pixel, "carne_de_mamute");
+            pixel.element = "carne_de_mamute";
             return;
         }
 
-        // comer grama
-        for (let dir of adjacentCoords) {
-            let x = pixel.x + dir[0];
-            let y = pixel.y + dir[1];
+        // Comer grama
+        for (let i = 0; i < adjacentCoords.length; i++) {
+            let x = pixel.x + adjacentCoords[i][0];
+            let y = pixel.y + adjacentCoords[i][1];
             if (outOfBounds(x,y)) continue;
             let other = pixelMap[x][y];
             if (other && other.element === "grass") {
-                changePixel(other, "air");
+                other.element = "air";
             }
         }
 
-        // reprodução no frio
+        // Reprodução no frio
         if (pixel.temp < 0 && Math.random() < 0.0005) {
-            let dir = adjacentCoords[Math.floor(Math.random()*adjacentCoords.length)];
+            let dir = adjacentCoords[Math.floor(Math.random() * adjacentCoords.length)];
             let x = pixel.x + dir[0];
             let y = pixel.y + dir[1];
             if (isEmpty(x,y)) {
